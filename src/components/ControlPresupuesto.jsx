d92ddacfb419e7c925757c76dd5728ff266d9233
anngelicafernanda <devanngelc@gmail.com>
2022-12-19 15:50:24 -0300

    Total Gastado y total disponible

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,31 +1,42 @@
+import { useEffect, useState } from "react";
 
+const ControlPresupuesto = ({ presupuesto, gastos }) => {
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
 
-const ControlPresupuesto = ({presupuesto}) => {
-    
-    const formatearCantidad = (cantidad) =>{
-       return cantidad.toLocaleString("en-US",{
-            style:"currency",
-            currency:"USD"
-        })
-    }
+    useEffect(() => {
+        const totalGastado = gastos.reduce(
+        (total, gasto) => gasto.cantidad + total,
+        0
+        );
+        const totalDisponible = presupuesto - totalGastado
+        setDisponible(totalDisponible)
+        setGastado(totalGastado)
+    }, [gastos]);
 
-    return (
+    const formatearCantidad = (cantidad) => {
+        return cantidad.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+        });
+    };
+
+  return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <p>Gráfica aquí</p>
       <div className="contenido-presupuesto">
         <p>
-            <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+          <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
         </p>
         <p>
-            <span>Disponible: </span> {formatearCantidad(0)}
+          <span>Disponible: </span> {formatearCantidad(disponible)}
         </p>
         <p>
-            <span>Gastado: </span> {formatearCantidad(0)}
+          <span>Gastado: </span> {formatearCantidad(gastado)}
         </p>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default ControlPresupuesto
+export default ControlPresupuesto;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,15 @@ const Header = ({
   presupuesto,
   setPresupuesto,
   isValidPresupuesto,
-  setIsValidPresupuesto,
+  setIsValidPresupuesto, gastos
 }) => {
   return (
     <header>
       <h1>Planificador de Gastos</h1>
       {isValidPresupuesto ? (
         <ControlPresupuesto
-        presupuesto={presupuesto}/>
+        presupuesto={presupuesto}
+        gastos={gastos}/>
       ) : (
         <NuevoPresupuesto
           presupuesto={presupuesto}
